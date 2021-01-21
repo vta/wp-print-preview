0293178fc6c0a383b8bb3f62c7bd3e1792b095a3
James Pham <jamespham93@yahoo.com>
2021-01-20 19:42:40 -0800

    WIP - custom MM field rendered. Need temps to display

--- gravityforms/class-wp-print-preview-mm-field.php
+++ gravityforms/class-wp-print-preview-mm-field.php
@@ -9,6 +9,7 @@
  * @subpackage Wp_Print_Preview/gravityforms
  * @author     James Pham <jamespham93@yahoo.com>
  * @see        https://awhitepixel.com/blog/tutorial-create-an-advanced-custom-gravity-forms-field-type-and-how-to-handle-multiple-input-values/
+ * @see        https://docs.gravityforms.com/gf_field/
  * @updated    1/20/2021
  */
 
@@ -16,7 +17,48 @@ if ( class_exists( 'GF_Field' ) ) {
 
     class MassMailerField extends GF_Field {
 
-        public $type = 'Mass Mailer';
+        public $type = 'mass_mailer';
+
+        /**
+         * Defines button label on GF admin field's panel.
+         * @return string
+         */
+        public function get_form_editor_field_title()
+        {
+            return 'Mailer Templates';
+        }
+
+        /**
+         * Places type of field
+         * @return array|string[]
+         */
+        public function get_form_editor_button()
+        {
+            return array(
+                'group' => 'advanced_fields',
+                'text'  => $this->get_form_editor_field_title(),
+            );
+        }
+
+        /**
+         * Displays predefined GF settings for our custom fields
+         * @return array|string[]
+         */
+        public function get_form_editor_field_settings()
+        {
+            return array(
+                'label_setting',
+                'choices_setting',
+                'description_setting',
+                'rules_setting',
+                'error_message_setting',
+                'css_class_setting',
+                'conditional_logic_field_setting',
+                'admin_label_setting'
+            );
+        }
+
+
 
     }
 
